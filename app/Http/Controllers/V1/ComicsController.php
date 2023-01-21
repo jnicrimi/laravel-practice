02445560bcab6d656da5586b88be1b81e2712da6
Junichiro Imai <jnicrimi@gmail.com>
2023-01-22 05:37:56 +0900

    feat: create comics controller

---
+++ app/Http/Controllers/V1/ComicsController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+
+class ComicsController extends Controller
+{
+    public function index(): array
+    {
+        // @TODO develop.
+        return [];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\ComicsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1', 'as' => 'v1.'], function () {
+    Route::group(['prefix' => 'comics', 'as' => 'comics.'], function () {
+        Route::get('/', [ComicsController::class, 'index'])->name('index');
+    });
 });
