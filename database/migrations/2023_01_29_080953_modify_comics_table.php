2620b7024ff12f14d95258b884fd254aeabebef9
Junichiro Imai <jnicrimi@gmail.com>
2023-01-29 17:23:59 +0900

    feat: migration modify comics table, add status

---
+++ database/migrations/2023_01_29_080953_modify_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            $table->enum('status', ['publish', 'draft', 'private'])
+                ->default('draft')
+                ->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
