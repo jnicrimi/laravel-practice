8f7be1af423206d2cd2f0e6d28f055ead14321fe
Junichiro Imai <jnicrimi@gmail.com>
2023-02-26 16:33:33 +0900

    feat: add comic domain test

---
+++ tests/Unit/Packages/Domain/ComicTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Packages\Domain\Comic;
+
+use Packages\Domain\Comic\Comic;
+use Packages\Domain\Comic\ComicId;
+use Packages\Domain\Comic\ComicStatus;
+use Packages\Domain\Comic\ComicStatusCase;
+use PHPUnit\Framework\TestCase;
+
+class ComicTest extends TestCase
+{
+    /**
+     * @var Comic
+     */
+    private $comic;
+
+    /**
+     * @var int
+     */
+    private $id = 1;
+
+    /**
+     * @var string
+     */
+    private $key = 'key';
+
+    /**
+     * @var string
+     */
+    private $name = 'name';
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->comic = new Comic(
+            new ComicId($this->id),
+            $this->key,
+            $this->name,
+            new ComicStatus(ComicStatusCase::PUBLISH)
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetId()
+    {
+        $this->assertInstanceOf(ComicId::class, $this->comic->getId());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetKey()
+    {
+        $this->assertSame($this->key, $this->comic->getKey());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetName()
+    {
+        $this->assertSame($this->name, $this->comic->getName());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetStatus()
+    {
+        $this->assertInstanceOf(ComicStatus::class, $this->comic->getStatus());
+    }
+}
