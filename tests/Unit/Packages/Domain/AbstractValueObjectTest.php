84c058b9f513313c7cb403f5b5cda6931de42ee2
Junichiro Imai <jnicrimi@gmail.com>
2023-03-15 20:06:45 +0900

    test: abstract value object

---
+++ tests/Unit/Packages/Domain/AbstractValueObjectTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Packages\Domain;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Packages\Domain\AbstractValueObject;
+use PHPUnit\Framework\TestCase;
+
+class AbstractValueObjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @return void
+     */
+    public function testGetValue()
+    {
+        $valueObject = $this->createValueObject();
+        $actual = $valueObject->getValue();
+        $expected = 'A';
+        $this->assertSame($actual, $expected);
+    }
+
+    /**
+     * @return AbstractValueObject
+     */
+    private function createValueObject(): AbstractValueObject
+    {
+        $value = 'A';
+
+        return new class ($value) extends AbstractValueObject {
+            protected function validate(): bool
+            {
+                return true;
+            }
+        };
+    }
+}
